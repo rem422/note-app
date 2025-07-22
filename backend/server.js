df2679c2b41f55e65a4ba88eb4ce7a4a85a2bb79
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2025-07-22 23:12:38 +0300

    Notes App

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+
 app.use(express.json());
 
 
-app.get("/", async (req, res) => {
-    
-})
+app.get("/", (req, res) => {
+    res.send("Hello There!");
+});
+
+app.listen(PORT,() => {
+    console.log(`Server started at:http://localhost:${PORT}`);
+});
