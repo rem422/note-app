08ded4cc790049899204f616675fe1036615b8cf
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2025-07-23 16:52:54 +0300

    Notes App

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,3 +1,90 @@
 import express from "express";
-import Notes from '../models/note.model.js';
+import Note from '../models/note.model.js';
 import { protect } from "../middleware/auth.js";
+
+const router = express.Router();
+
+// Get all notes
+router.get("/", protect, async (req, res) => {
+    try {
+        const notes = await Note.find({createdBy: req.user._id});
+        res.json(notes);
+    } catch(err) {
+        console.error("Get all notes error:", err);
+        res.status(500).json({message: "Server Error"});
+    }
+})
+
+// Create a note
+router.post("/", protect, async (req, res) => {
+    const { title, description } = req.body;
+    try {
+        if (!title || !description) {
+            return res.status(400).json({message: "Please fill all the fields"})
+        }
+
+        const note = await Note.create({ 
+            title, 
+            description, 
+            createdBy: req.user._id
+        });
+        res.status(201).json(note)
+    } catch(err) {
+        res.status(500).json({message: "Server Error"});
+    }
+});
+
+// Get a single note
+router.post("/:id", protect, async (req, res) => {
+    try {
+        const note = await Note.findById(req.params.id);
+        if(!note) {
+            return res.status(404).json({message: "Note not found"});
+        }
+    } catch(err) {
+        res.status(500).json({message: "Server Error"});
+    }
+});
+
+// Update a single note
+router.put("/:id", protect, async (req, res) => {
+    const { title, description } = req.body;
+    try {
+        const note = await Note.findById(req.params.id);
+        if(!note) {
+            return res.status(404).json({message: "Note not found"})
+        }
+
+        if (note.createdBy.toString() !== req.user._id.toString()) {
+            return res.status(401).json({message: "Not authorized"});
+        }
+
+        note.title = title || note.title;
+        note.description = description || note.description;
+
+        const UpdatedNote = await note.save();
+        res.status(200).json(UpdatedNote);
+    } catch(err) {
+        res.status(500).json({message: "Server Error"});
+    }
+});
+
+// Delete a note
+router.delete("/:id", protect, async (req, res) => {
+    try {
+        const note = await Note.findById(req.params.id);
+        if(!note) {
+            return res.status(404).json({message: "Note not found"})
+        }
+
+        if (note.createdBy.toString() !== req.user._id.toString()) {
+            return res.status(401).json({message: "Not authorized"});
+        }
+        await note.deleteOne();
+        res.json({message: 'Note deleted successfully'})
+    } catch(err) {
+        res.status(500).json({message: "Server Error"});
+    }
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import authRoutes from './routes/auth.js';
+import notesRoutes from './routes/notes.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/users", authRoutes);
+app.use("/api/notes", notesRoutes);
 
 app.listen(PORT,() => {
     console.log(`Server started at:http://localhost:${PORT}`);
